cf85178f9a6e5bec670350abc2a2d56b264f869b
Felipe <felipeo510.fs@gmail.com>
2017-10-18 16:55:46 -0700

    Belt review ready for deployment

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,9 @@ class Event < ActiveRecord::Base
   validates :name, presence: true, length: { in: 2..50 }
   validates :location, presence: true, length: { in: 4..25 }
   validates :state, presence: true, length: { in: 2..2 }
-  validates :date, inclusion: { in: (Date.today..Date.today+20.years), message: "must be in the future!"}
+  validates_each :date do |record, attr, value|
+    record.errors.add(attr, "must be in the future") if value < Date.today
+  end
 
   has_many :users
   has_many :joins, dependent: :destroy
